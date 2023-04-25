7f3c02c7800b3e783556994e423fedba19f9f346
TKaburu <traceymwendwa@gmail.com>
2023-04-25 02:51:39 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - Entry Point
+ * Description: Program that prints putchar
+ * Return: Always (0)
+ */
+int main(void)
+{
+	putchar('\n');
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: a function that prints the alphabet, in lowercase
+ * return: Always (0)
+ */
+void print_alphabet(void)
+{
+	char letter;
+
+	letter = 'a';
+	while (a <= 'z')
+	{
+		putchar(letter);
+		letter++
+	}
+}
+
+int main(void)
+{
+	ft_print_alphabet();
+	return (0);
+}
