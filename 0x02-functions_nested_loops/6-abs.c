34ca2ca01ca9ad0b1e82792f2dd06127417a85b1
TKaburu <traceymwendwa@gmail.com>
2023-04-25 19:24:25 -0700

    6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _abs - Gives the abolute value
+ * of an interger
+ *
+ * @: inputs number as an interger
+ *
+ * Return absolute value
+ */
+int _abs(int a)
+{
+	if (a >= 0)
+	{
+		return (a);
+	}
+	else
+	{
+		return (a * -1);
+	}
+}
