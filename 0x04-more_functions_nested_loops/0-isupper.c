218fc3d908bbc7202aa95fbc281a61f5c190a45a
TKaburu <traceymwendwa@gmail.com>
2023-04-27 05:11:30 -0700

    0-isupper.c

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _isupper - Uppercase character
+ * @c: char to check
+ *
+ * Return: 0 or 1
+ */
+
+int _isupper(int c)
+{
+	if (c <= 90)
+	{
+		return (1);
+	}
+	else
+
+	{
+		return (0);
+	}
+}
+
