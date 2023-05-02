420eb82dccdbc26197354c8b01c728f0086c66ce
TKaburu <traceymwendwa@gmail.com>
2023-05-02 14:42:44 -0700

    6-puts2.c

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -10,14 +10,14 @@ void puts2(char *str)
 	int t = 0;
 
 	while (str[t] != '\0')
-        {
+	{
 		if (t % 2 == 0)
 		{
 			_putchar(str[t]);
 		}
 		t++;
 
-        }
-        _putchar('\n');
+	}
+	_putchar('\n');
 }
 
