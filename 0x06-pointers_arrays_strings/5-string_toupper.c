c0fa2c71c1d5e95f2bd1505fbfd6c036027fecc7
TKaburu <traceymwendwa@gmail.com>
2023-05-04 11:20:28 -0700

    5-string_toupper.c

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -3,20 +3,20 @@
 /**
  * string_toupper - function that changes lower to uppercase
  * in a string
- * st: string
- * Return: string 
+ * @st: string
+ * Return: string
  */
 
 char *string_toupper(char *st)
 {
-        int t;
+	int t;
 
-        for (t = 0; st[t] != '\0'; t++)
-        {
-                if (st[t] >= 'a' && st[t] <= 'z')
-                {
-                        st[t] = st[t] - 32;
-                }
-        }
-        return (st);
+	for (t = 0; st[t] != '\0'; t++)
+	{
+		if (st[t] >= 'a' && st[t] <= 'z')
+		{
+			st[t] = st[t] - 32;
+		}
+	}
+	return (st);
 }
