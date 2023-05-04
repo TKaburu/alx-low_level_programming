a5a4b4745966993fa9a1f1ddacd541af8cfe965f
TKaburu <traceymwendwa@gmail.com>
2023-05-04 11:51:36 -0700

    1-strncat.c

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -14,7 +14,7 @@ char *_strncat(char *dest, char *src, int n)
 
 	len1 = strlen(dest);
 	for (t = 0; t < n && src[t] != '\0'; t++)
-        {
+	{
 		dest[len1 + t] = src[t];
 	}
 	dest[len1 + t] = '\0';
