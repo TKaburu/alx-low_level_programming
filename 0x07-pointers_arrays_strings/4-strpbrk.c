56adafb3cdf39c4c7e2ac2058fe25d5a9bc2bc6e
TKaburu <traceymwendwa@gmail.com>
2023-05-09 10:41:36 -0700

    4-strpbrk.c

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * _strpbrk - This function gets the set of
+ * bytes in any string
+ * @s: This is the string
+ * @accept: This is the input searched for
+ *
+ * Return: 0
+ */
+char *_strpbrk(char *s, char *accept)
+{
+	int t;
+
+	while (*s)
+	{
+		for (t = 0; accept[t]; t++)
+		{
+			if (*s == accept[t])
+				return (s);
+		}
+
+		
+		s++;
+	}
+	return ('\0');
+}
