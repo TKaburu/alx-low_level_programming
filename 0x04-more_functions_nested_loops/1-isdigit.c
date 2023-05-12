1249c3b76ecc9fc1eb6fd40a8d5ee0086e680053
Tracey Kaburu <128824824+TKaburu@users.noreply.github.com>
2023-05-12 08:55:29 +0300

    Update 1-isdigit.c

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * _isdigit - Checks for a number 0 through 9
+ * _isdigit - This function Checks for a number 0 through 9
  * @c: check if number is between 0 and 9
  * Return: 0 or 1
  */
