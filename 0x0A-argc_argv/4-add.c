a50fd03f821a2967cc9c0a523826b1177326ef42
TKaburu <traceymwendwa@gmail.com>
2023-05-17 03:26:47 -0700

    Adds positive number

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -3,34 +3,32 @@
 
 
 /**
- * main - This is the entry point fo the function
- *
- * Return: Always 0 (Success)
- */
+* main - This is the entry point fo the function
+* @argc: This is an argument for the function
+* @argv: This is a second argument for the function
+* Return: 0
+*/
 
 
 
 int main(int argc, char *argv[])
 {
-        int t = 1, sum = 0;
-
-        if (argc == 1)
-                printf("0\n");
+	int t = 1, sum = 0;
 
 	while (t < argc)
-        {
-                if (*argv[t] >= '0' && *argv[t] <= '9')
-                {
-                        sum += atoi(argv[t]);
-                }
-                else
-                {
-                        printf("Error\n");
-                        return (1);
-                }
+	{
+		if (*argv[t] >= '0' && *argv[t] <= '9')
+		{
+			sum += atoi(argv[t]);
+		}
+		else
+		{
+			printf("Error\n");
+			return (1);
+		}
 		t++;
-        }
-        printf("%d\n", sum);
-        return (0);
+	}
+	printf("%d\n", sum);
+	return (0);
 }
 
