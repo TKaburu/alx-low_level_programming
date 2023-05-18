c0b9b54a25c6970f2bb1a3254a831d682144c850
TKaburu <traceymwendwa@gmail.com>
2023-05-18 04:19:41 -0700

    Calloc

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ * _calloc - This function allocates memory of rray using
+ * malloc
+ * @nmeb: This is an unsigned int
+ * @size: This is an unsigned int
+ *
+ * Return: pointer otherwise NULL if it fails
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	unsigned int k;
+	char *t;
+	/* If nmemb or size is 0, then*/
+
+	if (nmemb == 0 || size == 0)
+	{
+		return (NULL);
+	}
+	
+	t = malloc(nmemb * size);
+	/*check if allocation of memory is succesful */
+	if (t == 0)
+	{
+		return (NULL);
+	}
+	k = 0;
+	/*sets value of the memory allocate*/
+
+	do {
+		*(t + 1) = 0;
+		k++;
+	} while (k < nmemb * size);
+
+	return (t);
+}
