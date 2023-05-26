afa218775479ff65b9cfa7651e8efcf8c01f24ce
TKaburu <traceymwendwa@gmail.com>
2023-05-25 23:53:06 -0700

    Task 3

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -1,26 +1,30 @@
 #include "3-calc.h"
 
 /**
- * get_op_func - This is a function pointer
- * @s: This is an operator
- *
- * Return: The pointer to the function
- */
+* get_op_func - This is a function pointer
+* @s: This is an operator
+*
+* Return: The pointer to the function
+*/
 
 int (*get_op_func(char *s))(int, int)
 {
 	op_t ops[] = {
-        {"+", op_add},
-        {"-", op_sub},
-        {"*", op_mul},
-        {"/", op_div},
-        {"%", op_mod},
-        {NULL, NULL}
-    };
-    int i = 0;
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}
+	};
+	int t = 0;
 
-	if (*s != ops[])
+	while (ops[t].f != NULL)
 	{
-		return (NULL);
+		if (*s == *(ops[t].op) && s[1] == '\0')
+			return (ops[t].f);
+		t++;
 	}
+	printf("Error\n");
+	exit(99);
 }
--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,5 +1,6 @@
 #include "3-calc.h"
 #include <stdlib.h>
+#include <string.h>
 
 /**
  * main - Entry point of the function
@@ -9,26 +10,35 @@
  * Return: 0
  */
 
-int main(int  argc, char *argv[])
-
+int main(int argc, char *argv[])
+{
+	int i, j;
 
 	if (argc < 4 || argc > 4)
 	{
-		printf("ERROR\n");
-		exit(42);
+		printf("Error\n");
+		return (98);
 	}
 
 	if ((*argv[2] != '+' && *argv[2] != '-'
 	&& *argv[2] != '*' && *argv[2] != '/'
-	&& *argv[2] != '%'))
+	&& *argv[2] != '%') || (strlen(argv[2]) > 1))
 	{
 		printf("Error\n");
-		return (98);
+		return (99);
+	}
+	
+	i = atoi(argv[1]);
+	j = atoi(argv[3]);
 
-	int t, k;
+	if ((*argv[2] == '/' || *argv[2] == '%')
+	&& *argv[3] == '0')
+	{
+		printf("Error\n");
+		return (100);
+	}
 
-	t= atoi(argv[1]);
-	k = atoi(argv[3]);
+	(*get_op_func(argv[2]))(i, j);
 
 	return (0);
 }
