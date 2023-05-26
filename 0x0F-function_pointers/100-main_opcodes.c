bdda034aa03cd2b339435081e9761a6ea8f18b50
TKaburu <traceymwendwa@gmail.com>
2023-05-25 23:07:16 -0700

    Opcodes

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -2,43 +2,45 @@
 #include <stdlib.h>
 
 /**
- * main - The function entry point
- * @argv: This is the array arguments
- * @argc: This is an argument
+ * main - Prints the opcodes of itself.
+ * @argc: The number of arguments supplied to the program.
+ * @argv: An array of pointers to the arguments.
  *
- * Return: 0
+ * Return: Always 0.
  */
-
 int main(int argc, char *argv[])
 {
-	int bytes, t;
-	char *k;
+	int t, k;
+	int (*address)(int, char **) = main;
+	unsigned char op;
 
 	if (argc < 2 || argc > 2)
 	{
 		printf("Error\n");
-		exit(42);
+		exit(1);
 	}
 
-	bytes = atoi(argv[1]);
+	t = atoi(argv[1]);
 
-	if (bytes < 0)
+	if (t < 0)
 	{
 		printf("Error\n");
-		exit(98);
+		exit(2);
 	}
-	k = (char *)main;
 
-	t = 0;
+	for (k = 0; k < t; k++)
+	{
+		op = *(unsigned char *)address;
+		printf("%.2x", op);
 
-	do {
-		if (t == bytes - 1)
-		{
-			printf("%02hhx\n", k[t]);
-			break;
-		}
-		printf("%02hhx ", k[t]);
-		t++;
-	} while (t < bytes);
+		if (k == t - 1)
+			continue;
+		printf(" ");
+
+		address++;
+	}
+
+	printf("\n");
+
 	return (0);
 }
