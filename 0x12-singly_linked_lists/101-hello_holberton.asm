f592f3fcf41ac587525dcea6e903b098ed6f4fbb
TKaburu <traceymwendwa@gmail.com>
2023-06-03 01:42:21 -0700

    Hello

---
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -0,0 +1,12 @@
+#include "lists.h"
+
+global    main
+          extern    printf
+main:
+	  mov   edi, format
+	  xor   eax, eax
+	  call  printf
+	  mov 	eax, 0
+	  ret
+format: db `Hello, Holberton\n`,0
+
