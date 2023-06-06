e970b052e44d420c1550b73747dc986d0637adaf
TKaburu <traceymwendwa@gmail.com>
2023-06-06 07:13:11 -0700

    Print list

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -12,11 +12,12 @@ size_t print_listint(const listint_t *h)
 {
 	size_t n = 0;
 
-	do {
+	while (h != NULL)
+	{
 		printf("%d\n", h->n);
-		n++;
 		h = h->next;
-	} while (h != NULL);
+		n++;
+	}
 	return (n);
 }
 
