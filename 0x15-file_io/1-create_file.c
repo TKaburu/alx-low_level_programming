eabfd9ee649390410832a6e2723c80af0c4da4cd
TKaburu <traceymwendwa@gmail.com>
2023-06-12 18:52:55 -0700

    Fixing output error

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,40 +1,37 @@
 #include "main.h"
-#include <string.h>
+#include <stdio.h>
 
 /**
- * create_file - This function creates a file
- * @filename: The file to be created
- * @text_content:  a NULL terminated string to write to the file
- *
- * Return: 1 on success, -1 on failure
- */
+* create_file - This function creates a file
+* @filename: Name of the file
+* @text_content: A NULL terminated string to write to the file
+*
+* Return: 1 if success or -1 if failure
+*/
 
 int create_file(const char *filename, char *text_content)
 {
-	int w, fd;
+	int w, fd, count = 0;
 
 	if (filename == NULL)
 	{
 		return (-1);
 	}
+	if (text_content != NULL)
+	{
+		do {
+			count++;
+		} while (text_content[count] != '\0');
+	}
 
-	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC | S_IRUSR | S_IWUSR);
-	if (fd == -1)
+	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	w = write(fd, text_content, count);
+
+	if (fd == -1 || w == -1)
 	{
 		return (-1);
 	}
-	if (text_content == NULL)
-	{
-		text_content = "";
-	}
-	else if (text_content != NULL)
-	{
-		w = write(fd, text_content, strlen(text_content));
-		if (w == -1)
-		{
-			close(fd);
-			return (-1);
-		}
-	}
+	close(fd);
+
 	return (1);
 }
