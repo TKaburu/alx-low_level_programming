a25a79752556d3d52d12616c1bed069b3e5eef0f
TKaburu <traceymwendwa@gmail.com>
2023-07-09 23:43:14 -0700

    Betty fixes

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,5 +1,14 @@
 #include "main.h"
 
+/**
+ * read_textfile - This function reads a text file and prints
+ * it to the POSIX standard output.
+ * @filename: This is a pointer to the file
+ * @letters: The letters tobe read and printed
+ *
+ * Return: number of letters function reads and prints otherwise 0
+ */
+
 
 ssize_t read_textfile(const char *filename, size_t letters)
 {
