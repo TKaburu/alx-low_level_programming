1046b05e3fa0d5622b98d5ccef0f4300eac743ab
Tracey Kaburu <128824824+TKaburu@users.noreply.github.com>
2023-10-16 20:55:07 +0300

    Create 5-strstr.c

---
+++ 0x18-dynamic_libraries/5-strstr.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * _strstr - This function locates substring
+ * @needle: This is a substring
+ * @haystack: This is the string
+ *
+ *Return: 0
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+	while (*haystack != '\0')
+	{
+		char *t = needle;
+		char *u = haystack;
+
+		for (; *u == *t && *t != '\0'; u++)
+		{
+			t++;
+		}
+		if (*t == '\0')
+		{
+			return (haystack);
+		}
+
+		haystack++;
+	}
+	return (0);
+}
