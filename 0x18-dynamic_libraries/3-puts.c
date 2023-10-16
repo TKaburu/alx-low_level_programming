7bbc49c311790f5ec75a40ec922bfb7ef708fb7d
Tracey Kaburu <128824824+TKaburu@users.noreply.github.com>
2023-10-16 20:51:33 +0300

    Create 3-puts.c

---
+++ 0x18-dynamic_libraries/3-puts.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _puts - This prints string to stdout
+ * @str: The string that should be printed
+ */
+
+void _puts(char *str)
+{
+	int w;
+
+	for (w = 0; str[w] != '\0'; w++)
+	{
+		_putchar(str[w]);
+	}
+	_putchar('\n');
+}
