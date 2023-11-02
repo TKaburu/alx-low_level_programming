11d53b8e131fccfa55737fea41afddb273af2d7c
TKaburu <traceymwendwa@gmail.com>
2023-11-01 22:01:29 -0700

    Fixing betty and spelling mistakes

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -1,17 +1,17 @@
 #include "hash_tables.h"
 
 /**
- * hash_table_create - Thi function creates a hash table
+ * hash_table_create - This function creates a hash table
  * @size: This is the size of the array
  *
- * Return: pointer to the newly created hash table otherwise Null
+ * Return: pointer to the newly created hash table otherwise NULL
  */
 
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	unsigned int count = 0;
 	hash_table_t *hash_table = malloc(sizeof(hash_table_t));
-	
+
 	/*chech if memory allocation was successful */
 	if (hash_table == NULL)
 		return (NULL);
@@ -22,6 +22,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 	/*chech if memory allocation was successful */
 	if (hash_table->array == NULL)
 		return (NULL);
+
 	while (count < size)
 	{
 		hash_table->array[count] = NULL;
