3f39e451c4fe4df84a0f2b1a2310fbe2eb9d7713
TKaburu <traceymwendwa@gmail.com>
2023-11-03 16:11:11 -0700

    fixed betty

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -18,7 +18,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	if (ht == NULL || value == NULL || key == NULL)
 		return (0);
 	len = ht->size;
-	index = key_index((const unsigned char *)key, size);
+	index = key_index((const unsigned char *)key, len);
 
 	temp = ht->array[index];
 	while (temp)
