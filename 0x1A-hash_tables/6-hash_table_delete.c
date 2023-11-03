8a2ebb5f2a534779286644a9e13f79faafd72087
TKaburu <traceymwendwa@gmail.com>
2023-11-03 12:15:23 -0700

    Delete the table

---
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -0,0 +1,28 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_delete - This function deletes the hash table
+ * @ht: The hash table to be deleted
+ *
+ */
+
+void hash_table_delete(hash_table_t *ht)
+{
+	unsigned int index = 0;
+	hash_node_t *node = ht->array[index];
+
+	if (ht == NULL)
+		return ;
+	for (; index < ht->size; ++index)
+	{
+		while (node)
+		{
+			/*node = node ->next;*/
+			free(node->key);
+			free(node->value);
+			free(node);
+		}
+	}
+	free(ht->array);
+	free(ht);
+}
